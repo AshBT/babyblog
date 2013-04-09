91a5db68ff356754fde011951b1222a59a146297
ashbt <ashley.beattie@gmail.com>
2013-04-09 18:25:22 -0400

    first commit

---
+++ baby_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyBlog::Application.config.secret_token = '9c467b5dcca7a6da7b8636362cf6de618c91d2f40b958d0592507bd109eddb5c346b327f5c935aa3e8b4baa1801b96ebd7201f4075ee86a551c87c4be8d36b88'
